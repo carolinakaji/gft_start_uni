125bb31e2e488fec8d7f66aa12cb50b51526f1cf
Carolina <carolina.kaji@gmail.com>
2020-09-23 09:18:08 -0300

    Ajuste no Teste 4

--- Teste_04/resultado.php
+++ Teste_04/resultado.php
@@ -7,15 +7,20 @@ $msgFinal = 'muito mais';
 $palavra = ' incomoda ';
 $msgTotal ='';
 
+// Verifica condição se o valor é maior que 0
 if($numero > 0){
+  // Loop de repetição para calcular quantas vezes a palavra incomoda aparecerá: iniciando de zero até o valor informado
   for($i=0; $i < $numero; $i++){
     $msgTotal .= $palavra;
   }
+  // Exibe a música em tela
   echo ($msgInicio . $msgTotal . $msgFinal);
+  // Verifica validade dos valores serem negativos ou vazio
 }elseif($numero === '' || $numero <= 0){
+  // Exibe mensagem de valores invalidos em tela
   echo 'Escreva um número válido';
 }
 echo "<p><a href='index.php'>Voltar</a></p>";
-// Loop de repetição para calcular quantas vezes a palavra incomoda aparecerá: iniciando de zero até o valor informado
+
 
 
